a19fbb5cf51552f538196d1e72853e854d8dd976
Joe Attardi <jattardi@gmail.com>
2026-04-12 17:04:30 -0400

    Add Fastify and minimal server example

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello from TypeScript!');
+import Fastify from 'fastify';
+
+const fastify = Fastify({
+    logger: true
+});
+
+fastify.get('/', () => {
+    return { hello: 'world' }
+});
+
+try {
+    await fastify.listen({ port: 3000 });
+} catch (error) {
+    fastify.log.error(error);
+    process.exit(1);
+}
