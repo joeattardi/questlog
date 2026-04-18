81c4c041107db7929502b5469a15540f0e19950e
Joe Attardi <jattardi@gmail.com>
2026-04-18 18:26:44 -0400

    Separate out protected game routes

---
+++ apps/backend/src/routes/game-routes.ts
@@ -0,0 +1,38 @@
+import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox';
+import { eq } from 'drizzle-orm';
+import { FastifyInstance } from 'fastify';
+import { Static, Type } from 'typebox';
+import { database } from '../database/database.js';
+import { games } from '../database/schema.js';
+
+const AddGameBody = Type.Object({
+    name: Type.String()
+});
+type AddGameBody = Static<typeof AddGameBody>;
+
+export async function gameRoutes(fastify: FastifyInstance) {
+    const app = fastify.withTypeProvider<TypeBoxTypeProvider>();
+
+    app.get('/api/games', async (request, reply) => {
+        const userGames = await database
+            .select()
+            .from(games)
+            .where(eq(games.userId, request.session!.user.id));
+        reply.send(userGames);
+    });
+
+    app.post('/api/games', {
+        schema: {
+            body: AddGameBody
+        }
+    }, async (request, reply) => {
+        const game = request.body;
+
+        await database.insert(games).values({
+            name: game.name,
+            userId: request.session!.user.id
+        });
+
+        reply.status(201).send({ message: 'Game added successfully' });
+    });
+}
--- apps/backend/src/routes/protected-routes.ts
+++ apps/backend/src/routes/protected-routes.ts
@@ -1,17 +1,7 @@
 import { fromNodeHeaders } from 'better-auth/node';
 import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 import { auth } from '../auth/auth.js';
-import { database } from '../database/database.js';
-import { games } from '../database/schema.js';
-
-import { Type, Static } from 'typebox';
-import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox';
-import { eq } from 'drizzle-orm';
-
-const AddGameBody = Type.Object({
-    name: Type.String()
-});
-type AddGameBody = Static<typeof AddGameBody>;
+import { gameRoutes } from './game-routes.js';
 
 async function getSession(request: FastifyRequest, reply: FastifyReply) {
     const session = await auth.api.getSession({
@@ -29,26 +19,6 @@ async function getSession(request: FastifyRequest, reply: FastifyReply) {
 
 export async function protectedRoutes(fastify: FastifyInstance) {
     fastify.addHook('preHandler', getSession);
-    const app = fastify.withTypeProvider<TypeBoxTypeProvider>();
 
-    fastify.get('/api/games', async (request, reply) => {
-        const userGames = await database.select().from(games)
-            .where(eq(games.userId, request.session!.user.id));
-        reply.send(userGames);
-    });
-
-    app.post('/api/games', {
-        schema: {
-            body: AddGameBody
-        }
-    }, async (request, reply) => {
-        const game = request.body;
-
-        await database.insert(games).values({
-            name: game.name,
-            userId: request.session!.user.id
-        });
-
-        reply.status(201).send({ message: 'Game added successfully' });
-    });
+    fastify.register(gameRoutes);
 }
